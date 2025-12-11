f19557c3a5cdaf9a55f552b818fef508255ec0cf
Arezou-Raz <arezou.rzghn@gmail.com>
2025-12-11 13:23:55 +0000

    Solution

--- part04-20_star_studded/src/star_studded.py
+++
@@ -1,6 +0,0 @@
-
-word = input("Please type in a string: ")
-
-for character in word:
-    print(character)
-    print("*")
--- part04-21_negative_to_positive/src/negative_to_positive.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Please type in a positive integer: "))
-
-for i in range(-number, number+1):
-    if i == 0:
-        continue
-    else:
-        print(i)
