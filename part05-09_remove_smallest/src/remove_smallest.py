83a4b47dda70630c43ea89db5f64fee5313721bc
Arezou-Raz <arezou.rzghn@gmail.com>
2025-12-23 20:06:30 +0000

    Tue, Dec 23, 2025, 8:06 PM +00:00

---
+++ part05-09_remove_smallest/src/remove_smallest.py
@@ -0,0 +1,11 @@
+def remove_smallest( my_list : list):
+    smallest = my_list[0]
+    for i in my_list:
+        if i < smallest:
+            smallest = i
+    my_list.remove(smallest)
+
+if __name__ == "__main__":
+    numbers = [2, 4, 5]
+    remove_smallest(numbers)
+    print(numbers)
